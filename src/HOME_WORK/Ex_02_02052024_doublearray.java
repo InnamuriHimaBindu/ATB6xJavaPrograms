5ae63e37a8f92b9519d41c848e169276825c3cb1
InnamuriHimaBindu <hima111.innamuri@gmail.com>
2024-05-03 19:45:27 +0530

    add

---
+++ src/HOME_WORK/Ex_02_02052024_doublearray.java
@@ -0,0 +1,35 @@
+package HOME_WORK;
+
+import java.util.Scanner;
+
+public class Ex_02_02052024_doublearray
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] arr=new int[3];
+        int arr1;
+        System.out.println("Enter 1st number");
+        arr[0]=sc.nextInt();
+
+        System.out.println("Enter 2nd number");
+        arr[1]=sc.nextInt();
+
+        System.out.println("Enter 3rd number");
+        arr[2]=sc.nextInt();
+
+
+        for(int i=0;i<arr.length;i++)
+        {
+             arr1=arr[i]+arr[i];
+             int arr2=arr[i]*arr[i];
+            System.out.println(arr1);
+            System.out.println("multiple->" +arr2);
+        }
+
+      // int[] arr2=new int[1];
+     /*   for(int i=0;i<arr.length;i++)
+        {
+           arr2=arr[i]+arr[i];
+        }*/
+    }
+}
---
+++ src/HOME_WORK/Ex_04_02052024_maxvalue.java
@@ -0,0 +1,16 @@
+package HOME_WORK;
+
+public class Ex_04_02052024_maxvalue {
+    public static void main(String[] args) {
+
+        int[] arr={30,50,60,90,10,100,90,200};
+        int max=arr[0];
+        for(int i=0;i<arr.length ; i++)
+        {
+            if(arr[i]>max)
+                max=arr[i];
+        }
+        System.out.println("maximum valus:" +max);
+    }
+
+}
